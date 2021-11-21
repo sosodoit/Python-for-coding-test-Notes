c15c96dcad691aa2e992ad2cb1b4e991e74780f1
김소희 <rosesh0218@gmail.com>
2021-11-21 10:48:37 +0900

    Create 02시각.py

---
+++ Implementation/02시각.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+count = 0
+for h in range(n+1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h) + str(m) + str(s):
+                count += 1
+print(count)
+
+'''
+입력 5
+결과 11475
+'''
